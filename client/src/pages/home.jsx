4a99adda7fbedbeee3a21b8d865a69ec8f8d22d8
Marcos Eduardo Mori Kaminski <marcosemk1005@gmail.com>
2023-09-18 11:27:09 -0300

    .

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,5 +1,10 @@
+import { useState } from 'react';
+import axios from '@/utils/axiosConfig';
+
 export default function HomePage() 
 {
+  const [courses, setCourses] = useState([]);
+  
   return (
     <div>
       <h1>Home</h1>
